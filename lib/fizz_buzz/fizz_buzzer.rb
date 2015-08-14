1365ec1507b0c689cd6e4c46d1b3a54728af8ab5
Jack Nolan <jack.nolan@code42.com>
2015-08-13 21:08:30 -0500

    jnolan: refactor fizz buzzer

--- lib/fizz_buzz/fizz_buzzer.rb
+++ lib/fizz_buzz/fizz_buzzer.rb
@@ -4,20 +4,25 @@ class FizzBuzzer
 
   # Accepts an number and returns it fizzbuzzed
   def fizz_or_buzz(number)
-    if divisable?(number, 15)
-      FIZZ + BUZZ
-    elsif divisable?(number, 3)
-      FIZZ
-    elsif divisable?(number, 5)
-      BUZZ
-    else
-      number.to_s
-    end
+    fizz_buzz_string(number) || number.to_s
   end
 
   private
 
-  def divisable?(numerator, denominator)
+  def fizz_buzz_string(number)
+    string = "#{fizz(number)}#{buzz(number)}"
+    string.empty? ? nil : string
+  end
+
+  def fizz(number)
+    FIZZ if divisible?(number, 3)
+  end
+
+  def buzz(number)
+    BUZZ if divisible?(number, 5)
+  end
+
+  def divisible?(numerator, denominator)
     (numerator % denominator) == 0
   end
 end
