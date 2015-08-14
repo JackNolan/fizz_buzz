24416d26e1ae263a5e1f76ad962cfa61f9d55f1f
Jack Nolan <jack.nolan@code42.com>
2015-08-13 20:54:30 -0500

    jnolan: refactor FizzBuzz unit tests to remove dependency on STDOUT

--- spec/unit/fizz_buzz_spec.rb
+++ spec/unit/fizz_buzz_spec.rb
@@ -1,24 +1,22 @@
 require 'spec_helper'
 
 describe FizzBuzz do
-  let(:fizz_buzz) { FizzBuzz.new }
+  let(:test_output) { TestingOutput.new }
+  let(:fizz_buzz)   { FizzBuzz.new(output: test_output) }
   describe '#display' do
     it 'prints fizz or buzz a set of given number' do
-      expect(STDOUT).to receive(:puts).with('Fizz').ordered
-      expect(STDOUT).to receive(:puts).with('Buzz').ordered
-      expect(STDOUT).to receive(:puts).with('FizzBuzz').ordered
-      expect(STDOUT).to receive(:puts).with('7').ordered
-
       allow(fizz_buzz.fizz_buzzer).to receive(:fizz_or_buzz).with(3).and_return('Fizz')
       allow(fizz_buzz.fizz_buzzer).to receive(:fizz_or_buzz).with(5).and_return('Buzz')
       allow(fizz_buzz.fizz_buzzer).to receive(:fizz_or_buzz).with(15).and_return('FizzBuzz')
       allow(fizz_buzz.fizz_buzzer).to receive(:fizz_or_buzz).with(7).and_return('7')
 
       fizz_buzz.display([3, 5, 15, 7])
+
+      expect(test_output.get_writes).to eq %w(Fizz Buzz FizzBuzz 7)
     end
 
     it 'accepts a range of numbers' do
-      expect(STDOUT).to receive(:puts).with('Fizz').exactly(6).times
+      expect(test_output).to receive(:puts).with('Fizz').exactly(6).times
       allow(fizz_buzz.fizz_buzzer).to receive(:fizz_or_buzz).and_return('Fizz')
 
       fizz_buzz.display(1..6)
